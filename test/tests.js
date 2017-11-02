17a2ca6842cc99496a2a37a90e64b40faf9739c2
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-11-02 14:28:36 -0600

    everything is working

--- test/tests.js
+++ test/tests.js
@@ -1,24 +1,22 @@
 const expect = chai.expect;
 
 
+var testArr = [[{wind:{speed:10}}],[{}]]
 
 describe('toMPH', function(){
-  it('returns a number', function(){
+  it('returns a wind speed in MPH', function(){
     expect(toMPH(10)).to.equal(22)
   })
 })
 
 describe('toFahrenheit', function(){
-  it('returns degrees in Fahrenheit', function(){
+  it('returns temperature in Fahrenheit', function(){
     expect(toFahrenheit(280)).to.equal(44)
   })
 })
 
-
-var testArr = [[{wind:{speed:10}}],[{}]]
-
 describe('buildWindArray', function(){
   it('fills an empty array with wind speeds of resorts', function(){
     expect(Array.isArray(buildWindArray(['vail'],testArr))).to.equal(true)
-})
+  })
 })
